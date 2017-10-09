a360dff27a57e5868fce8b74a896648d134af6e7
Mikko Särkkä <mikkosar@uef.fi>
2017-10-09 13:01:35 +0300

    Make valid_args usable on vectors

--- common_funcs.R
+++ common_funcs.R
@@ -1,29 +1,31 @@
 
 # Check argument for zero length, NULL, NaN or NA value
-valid_arg <- function(arg, expected_class=NULL, expected_length=1, stop_on_false=TRUE) {
-  if(length(arg) != expected_length) {
-    if(stop_on_false) {
-      stop(paste0("Argument is of length ", length(arg), " instead of expected length ", expected_length, "."))
-    }
-    return (FALSE)
-  }
-  if(is.na(arg)) {
-    if(stop_on_false) {
-      stop("Argument is NA.")
-    }
-    return (FALSE)
-  }
+valid_arg <- function(arg, expected_class=NULL, expected_length=NULL, stop_on_false=TRUE) {
   if(is.null(arg)) {
     if(stop_on_false) {
       stop("Argument is NULL.")
     }
     return (FALSE)
   }
-  if(is.nan(arg)) {
+  if(!is.null(expected_length) && length(arg) != expected_length) {
     if(stop_on_false) {
-      stop("Argument is NULL.")
+      stop(paste0("Argument is of length ", length(arg), " instead of expected length ", expected_length, "."))
     }
     return (FALSE)
+  }
+  if(!isS4(arg)) {
+    if(any(is.na(arg))) {
+      if(stop_on_false) {
+        stop("Argument is or contains NA.")
+      }
+      return (FALSE)
+    }
+    # if(any(is.nan(arg))) {
+    #   if(stop_on_false) {
+    #     stop("Argument is or contains NaN.")
+    #   }
+    #   return (FALSE)
+    # }
   }
   if(!is.null(expected_class) && class(arg) != expected_class) {
     if(stop_on_false) {
